dc87822d002453d5fe2e3d6eacc5583100ca1636
feliippeee <felipe.tecnosuporte@gmail.com>
2022-06-05 18:29:27 -0300

    Init commit

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/feliippeee.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Felipe Marques</strong>
+                            <time title="9 de Maio às 08:15h" dataTime="2022-05-11 08:15:30">Cerca de 1h atrás</time>
+                            
+                        </div>
+
+                        <button title="Deletar Comentário">
+                            <Trash sizer={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bem dev !!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
